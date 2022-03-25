b64f91f2da0f362d4cdf4f68c72cd3fffc891276
Alisher it <develepor@mail.ru>
2022-03-25 15:25:01 +0600

    bag fix scrool

--- wolt 0.0.2/js/main.js
+++ wolt 0.0.2/js/main.js
@@ -1,8 +1,8 @@
 $(function(){
     $('.burger-menu').on('click', '.bar', function() {
+        $('body').toggleClass('lock');
         $('.menu-burger').slideToggle();
         $('.bar').toggleClass('change');
         $('.menu-burger__burger li').slideRight();
-        $('.menu-burger').toggleClass('lock');
     });
 });
