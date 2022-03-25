e42df131d049fd0716410c90c58c934228ab8025
Alisher it <develepor@mail.ru>
2022-03-25 15:56:42 +0600

    version slider 0.0.1

--- wolt 0.0.2/js/main.js
+++ wolt 0.0.2/js/main.js
@@ -5,4 +5,41 @@ $(function(){
         $('.bar').toggleClass('change');
         $('.menu-burger__burger li').slideRight();
     });
+
+
+    $('.work__column').slick({
+        slidesToShow: 3,
+        // arrows: false,
+        // autoplay: true,
+        // autoplaySpeed: 500,
+        // speed: 5000,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    });
 });
