d38c4d2b3edff3123ea7182c5b13c1c6c6cbf39b
Alisher it <develepor@mail.ru>
2022-04-24 11:51:00 +0600

    it-spk.kz

--- learn.JS/script.js
+++ learn.JS/script.js
@@ -1,1 +1,27 @@
-var str = ""
+document.querySelector('button').addEventListener('click', function(event){
+
+    var value = document.querySelector('input').value;
+
+    var obj = {
+        text: value
+    };
+
+
+
+    localStorage.setItem('headerText', JSON.stringify(obj));
+
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    var obj = {};
+
+    try{
+        var obj = JSON.parse(localStorage.getItem('headerText'));
+    } catch (e){}
+    
+    if(obj && obj.text && obj.text.trim()){
+        document.querySelector('h1').textContent = obj.text;
+    }
+
+})
